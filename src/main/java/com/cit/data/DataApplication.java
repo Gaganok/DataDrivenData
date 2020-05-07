ee1844ceb159a9f7b37b18d250d241ab314df926
Oleh <oleh.kepsha@mycit.ie>
2020-05-07 16:05:45 +0100

    Deleted file option

--- src/main/java/com/cit/data/DataApplication.java
+++ src/main/java/com/cit/data/DataApplication.java
@@ -12,17 +12,17 @@ import java.io.*;
 public class DataApplication {
 	private static String fileArg = "";
 	public static void main(String[] args) {
-		if(args.length > 0)
-			fileArg = args[0];
+		/*if(args.length > 0)
+			fileArg = args[0];*/
 		SpringApplication.run(DataApplication.class, args);
 	}
 
 	@Autowired private TestDataComponent tdc;
 	@PostConstruct
 	private void postConstruct() throws FileNotFoundException {
-		Reader reader = fileArg.isEmpty() ?  new StringReader(tdc.getTestData()) :  new FileReader(new File(fileArg));
+		Reader reader = new StringReader(tdc.getTestData());//fileArg.isEmpty() ?  new StringReader(tdc.getTestData()) :  new FileReader(new File(fileArg));
 		BufferedReader bf = new BufferedReader(reader);
-		System.out.println(tdc.getTestData());
+		//System.out.println(tdc.getTestData());
 		tdc.testFileData = bf;
 	}
 }
